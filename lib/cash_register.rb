68f3fba57a100f505bc6531beacf1fe0970fd2de
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-07 01:32:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,8 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += price * quantity
     quantity.times do
-    @items << title
-  end
+      @items << title
+    end
   end
 
   def apply_discount
