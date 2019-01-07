85226595d79f1d00677af7325f893d54a8fbe633
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-07 01:18:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
        @total = @total - (@total * @discount/100)
        "After the discount, the total comes to $#{@total}."
      else "There is no discount to apply."
-     end 
+     end
   end
 
 end
